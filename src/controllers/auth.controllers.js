58dfb927b63e639c8e709a14d21a7adf446cdbe2
Maximiliano Pérez <maximiliano.leonel.perez1999@gmail.com>
2023-06-16 19:03:11 -0300

    Agrego jwt y rutas para verificar el token y acceso

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,6 +1,42 @@
 import User from "../models/user.models.js";
+import { createAccessToken } from "../libs/jwt.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { TOKEN_SECRET } from "../config.js";
 
-export const login = async (req, res) => {};
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const userFound = await User.findOne({ email });
+
+    if (!userFound) {
+      return res.json({ message: "El email no existe" });
+    }
+
+    const isMatch = await bcrypt.compare(password, userFound.password);
+
+    if (!isMatch) {
+      return res.json({ message: "La contraseña es inválida" });
+    }
+
+    const token = await createAccessToken({
+      id: userFound._id,
+      username: userFound.username,
+    });
+
+    res.cookie("token", token);
+
+    res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      fullname: userFound.fullname,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 export const register = async (req, res) => {
   const { fullname, email, password, username } = req.body;
@@ -13,23 +49,61 @@ export const register = async (req, res) => {
       return res.json({ message: "Ya existe un usuario con ese email" });
     }
 
-    // Creamos un nuevos usuario
+    // Encrptar contraseña
+    const encryptedPassword = await bcrypt.hash(password, 10);
 
+    // Creamos un nuevos usuario
     const newUser = new User({
       fullname,
       email,
-      password,
+      password: encryptedPassword,
       username,
     });
 
+    // Guardo en base de datos
     const savedUser = await newUser.save();
 
-    return res.json({ message: "Usuario creado", savedUser });
+    // Creo el token
+    const token = await createAccessToken({ id: savedUser._id });
+
+    res.cookie("token", token);
+    res.json({
+      id: savedUser._id,
+      username: savedUser.username,
+      email: savedUser.email,
+      fullname: savedUser.fullname,
+    });
   } catch (error) {
-    console.log(error);
+    res.status(500).json({ message: error.message });
   }
 };
 
-export const logout = async (req, res) => {};
+export const logout = async (req, res) => {
+  res.cookie("token", "");
+  return res.status(200).json({ message: "Token eliminado" });
+};
 
-export const verifyToken = async (req, res) => {};
+export const verifyToken = async (req, res) => {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return res.send(false);
+  }
+
+  jwt.verify(token, TOKEN_SECRET, async (error, user) => {
+    if (error) return res.sendStatus(401);
+
+    const userFound = await User.findOne(user.id);
+
+    if (!userFound) {
+      return res.sendStatus(401);
+    }
+
+    return res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      fullname: userFound.username,
+    });
+  });
+};
--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -5,12 +5,13 @@ import {
   logout,
   verifyToken,
 } from "../controllers/auth.controllers.js";
+import {} from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.post("/login", login);
 router.post("/register", register);
-router.post("/logout", logout);
-router.post("/verify", verifyToken);
+router.post("/logout", verifyToken, logout);
+router.get("/verify", verifyToken);
 
 export default router;
