054e0463a94758fec6d59860aea42264bd828a79
Maximiliano Pérez <maximiliano.leonel.perez1999@gmail.com>
2023-06-24 11:10:50 -0300

    Agrego variables de entorno para el deploy

--- src/config.js
+++ src/config.js
@@ -8,4 +8,5 @@ export const MONGODB_URI =
   process.env.MONGODB_URI ||
   `mongodb+srv://maximiliano:${process.env.MONGODB_PASSWORD}@cluster0.ulu4jdk.mongodb.net/post-it-app?retryWrites=true&w=majority`;
 
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:4000";
+export const FRONTEND_URL =
+  process.env.FRONTEND_URL || process.env.LOCALHOST_URL;
