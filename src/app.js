fdad87b20877344b686dccac1cf8ec7759f911d4
Maximiliano Pérez <maximiliano.leonel.perez1999@gmail.com>
2023-06-24 11:52:03 -0300

    Configurando cors

--- src/app.js
+++ src/app.js
@@ -13,9 +13,16 @@ app.use(
   })
 );
 app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Credentials", true);
   next();
 });
+
 app.use(cookieParser());
 
 app.use("/api", authRouter);
