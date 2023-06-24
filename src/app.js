1529465dc51185eb4a835a4e46f71ddecb7b42c8
Maximiliano Pérez <maximiliano.leonel.perez1999@gmail.com>
2023-06-24 11:42:15 -0300

    Configurando credenciales cors

--- src/app.js
+++ src/app.js
@@ -7,10 +7,15 @@ import cookieParser from "cookie-parser";
 const app = express();
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 app.use(express.json());
 app.use(cookieParser());
 
