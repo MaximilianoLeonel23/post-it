50aedc5cab7891662587b64564bace3ef1393421
Maximiliano Pérez <maximiliano.leonel.perez1999@gmail.com>
2023-06-26 20:01:37 -0300

    Agrego middleware headers

--- src/app.js
+++ src/app.js
@@ -12,11 +12,15 @@ const app = express();
 // );
 
 app.use(express.json());
-
 app.use(cookieParser());
-app.use(
-  cors({ origin: "https://post-it-app-gilt.vercel.app", credentials: true })
-);
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 app.use("/api", authRouter);
 app.use("/api", postitRouter);
