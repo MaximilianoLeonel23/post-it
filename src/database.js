4eb2b4562c9a80293deed0dc9ce940e0cb3fdee4
Maximiliano Pérez <maximiliano.leonel.perez1999@gmail.com>
2023-07-14 16:03:51 -0300

    Agrego useNewUrlParse en db

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ import { MONGODB_URI } from "./config.js";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(MONGODB_URI);
+    await mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
   } catch (error) {
     console.log(error);
   }
