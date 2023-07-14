8aac0d7e2ebd1db4ae92f69864e6a1d3dc68bc2e
Maximiliano Pérez <maximiliano.leonel.perez1999@gmail.com>
2023-07-14 16:00:03 -0300

    Cambio de puerto

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
-export const PORT = process.env.PORT || 4000;
+export const PORT = process.env.PORT || 3030;
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 // export const MONGODB_URI =
 //   process.env.MONGODB_URI || "mongodb://127.0.0.1/post-it-app";
